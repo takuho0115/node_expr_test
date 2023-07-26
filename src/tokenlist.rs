562ad939ec57da95d7bd8685d4d4d77633aad9e8
takuho0115 <kiichi.nakai0115@gmail.com>
2023-07-26 09:11:56 +0900

    省メモリ化

--- src/tokenlist.rs
+++ src/tokenlist.rs
@@ -8,46 +8,12 @@ use token::*;
 pub struct TokenList{
 	pub original: String,
 	pub list: Vec<Token>,
-	pub current: Option<usize>,
+	pub current: usize,
 }
 
 impl TokenList{
 	pub fn new(s: impl Into<String>)->Self{
-	 	Self { original: s.into(), list: Vec::new(), current: None }.tokenize()
-	}
-
-	fn tokenize(mut self)->Self{
-		let mut chars = self.original.chars().enumerate().peekable();
-		let is_oparator = |c: &char| "+-*/();".contains(*c);
-		let is_comparator = |c: &char| "!=<>".contains(*c);
-		let is_alphabets = |c: &char| String::from_utf8((b'A'..=b'z').collect()).unwrap().contains(*c);
-		let is_digit = |c: &char| !c.to_digit(10).is_none();
-		
-		// while chars.next() != None
-		'o:while let Some((i, c)) = chars.next() {
-			if c.is_whitespace(){
-				continue;
-			}
-			for reader in [is_oparator,is_comparator,is_alphabets]{
-				if reader(&c){
-					let mut tok = Token::new(TokenKind::TkReserved, &c, &i);
-					tok.str = Some(Self::read_ahead(&c, &mut chars, reader));
-					self.list.push(tok);
-					continue 'o;
-				}
-			}
-			if is_digit(&c){
-				let mut tok = Token::new(TokenKind::TkNum, &c, &i);
-				tok.val = Some(Self::read_ahead(&c, &mut chars, is_digit)
-					.parse::<usize>().expect("out of range"));
-				self.list.push(tok);
-				continue;
-			}
-			self.at_error(i, "トークナイズできません。");
-		}
-
-		self.list.push(Token::new(TokenKind::TkEof, &"\0", &chars.count()));
-		self
+	 	Self { original: s.into(), list: Vec::new(), current: 0 }
 	}
 
 	fn at_error(&self, pos: usize, e_message: &str){
@@ -74,13 +40,34 @@ impl TokenList{
 impl Iterator for TokenList {
 	type Item = Token;
 	fn next(&mut self) -> Option<Self::Item> {
-		self.current = 
-		if self.current.is_none(){
-			Some(0)
-		}else{
-			Some(self.current.unwrap() + 1)
-		};
+		let is_oparator = |c: &char| "+-*/();".contains(*c);
+		let is_comparator = |c: &char| "!=<>".contains(*c);
+		let is_alphabets = |c: &char| String::from_utf8((b'A'..=b'z').collect()).unwrap().contains(*c);
+		let is_digit = |c: &char| !c.to_digit(10).is_none();
 
-		self.list.get(self.current.unwrap()).cloned()
+		let mut chars = self.original.chars().enumerate().peekable();
+
+		
+		while let Some((i, c)) = chars.nth(self.current + 1) {
+			self.current += 1;
+			if c.is_whitespace(){
+				continue;
+			}
+			for reader in [is_oparator,is_comparator,is_alphabets]{
+				if reader(&c){
+					let mut tok = Token::new(TokenKind::TkReserved, &c, &i);
+					tok.str = Some(Self::read_ahead(&c, &mut chars, reader));
+					return Some(tok);
+				}
+			}
+			if is_digit(&c){
+				let mut tok = Token::new(TokenKind::TkNum, &c, &i);
+				tok.val = Some(Self::read_ahead(&c, &mut chars, is_digit)
+					.parse::<usize>().expect("out of range"));
+				return Some(tok);
+			}
+			self.at_error(i, "トークナイズできません。");
+		}
+		return Some(Token::new(TokenKind::TkEof, &"\0", &chars.count()));
 	}
 }
